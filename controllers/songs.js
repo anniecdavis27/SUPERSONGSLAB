795a7bff0f8deb82f2a2e7493fb3646e4751704f
M. Brock Podgurski <mbpod10@gmail.com>
2020-07-16 12:27:13 -0400

    commit before deploy

--- controllers/songs.js
+++ controllers/songs.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Song = require("../models/Song");
+const { json } = require("express");
 
 //all songs
 router.get("/", (req, res) => {
@@ -17,6 +18,22 @@ router.get("/:id", (req, res) => {
     else res.json(song);
   });
 });
+// get by title
+router.get("/title/:title", (req, res) => {
+  Song.find({ title: req.params.title }, (error, title) => {
+    if (error) console.log(error);
+    else res.json(title);
+  });
+});
+
+//get by artist name
+router.get("/artist/:artist", (req, res) => {
+  Song.find({ artist: req.params.artist }, (error, title) => {
+    if (error) console.log(error);
+    else res.json(title);
+  });
+});
+
 //post new song
 router.post("/", (req, res) => {
   Song.create(req.body, (error, song) => {
@@ -59,18 +76,6 @@ router.put("/:id/fav", (req, res) => {
   );
 });
 
-//add to favs
-router.put("/:id/fav", (req, res) => {
-  Song.findOneAndUpdate(
-    { _id: req.params.id },
-    { isFavorite: true },
-    { new: true },
-    (error, song) => {
-      if (error) console.log(error);
-      else res.json(song);
-    }
-  );
-});
 // remove favs
 router.put("/:id/fav/remove", (req, res) => {
   Song.findOneAndUpdate(
