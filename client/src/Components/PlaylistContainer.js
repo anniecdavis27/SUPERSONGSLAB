ac54b6072f3ce407d5b14b06b45c137a7e653e20
Annie Davis <anniecdavis27@gmail.com>
2020-07-15 23:22:17 -0400

    get songs working

--- client/src/Components/PlaylistContainer.js
+++ client/src/Components/PlaylistContainer.js
@@ -1,17 +1,22 @@
 import React, {useState, useEffect} from 'react';
 import '../App.css';
+import apiUrl from '../apiConfig'
+import axios from 'axios'
 import Songs from './Songs'
-import Faves from './Faves'
+// import Faves from './Faves'
 
 function PlaylistContainer() {
     const [songs, setSongs] = useState([])
     const [fave, setFaves] = useState([])
+
+console.log(apiUrl)
 
     useEffect(() => {
       const makeAPICall = async () => {
         try {
           const response = await axios(`${apiUrl}/songs`)
           setSongs(response.data)
+          console.log(response.data)
         } catch (err) {
           console.error(err)
         }
@@ -19,26 +24,45 @@ function PlaylistContainer() {
       makeAPICall()
     }, [])
 
-    useEffect(() => {
-        const makeAPICall = async () => {
-          try {
-            const response = await axios(`${apiUrl}/fav`)
-            setFaves(response.data)
-          } catch (err) {
-            console.error(err)
-          }
-        }
-        makeAPICall()
-      }, [])
+    console.log(songs)
+
+        if(!songs) {
+        return <p>...loading</p>
+    }
+
+    let songList = songs.map(song => (
+        <li key={song.id}>
+            <h3>{song.title}</h3>
+            <p>{song.time}</p>
+            <p>{song.artist}</p>
+            <button>Edit Song</button>
+            <button>Remove Song</button>
+            <button>Add to Favorites</button>
+
+        </li>
+    ))
+
+    // useEffect(() => {
+    //     const makeAPICall = async () => {
+    //       try {
+    //         const response = await axios(`${apiUrl}/fav`)
+    //         setFaves(response.data)
+    //       } catch (err) {
+    //         console.error(err)
+    //       }
+    //     }
+    //     makeAPICall()
+    //   }, [])
 
   return (
     <div className="playlist-container">
         <h1>Playlist</h1>
-        <Songs songs={songs}/>
+        <ul>{songList}</ul>
+        {/* <Songs songs={songs}/> */}
         <button>Add a Song</button>
         <h1>Favorites</h1>
-        <Faves faves={faves}/>
-        <Form />
+        {/* <Faves faves={faves}/> */}
+        {/* <Form /> */}
     </div>
   );
 }
--- client/src/Components/Songs.js
+++ client/src/Components/Songs.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import '../App.css';
 
-function Songs({ songs }) {
+function Songs({songs}) {
 
-    if(!songs) {
-        return <p>...loading</p>
-    }
-    let songList = songs.map(song => {
-        <div key={song.id}>
-            <h3>{song.title}</h3>
-            <p>{song.time}</p>
-            <p>{song.artist}</p>
-            <button>Edit Song</button>
-            <button>Remove Song</button>
-            <button>Add to Favorites</button>
+    console.log(songs)
 
-        </div>
-    })
+    // if(!songs) {
+    //     return <p>...loading</p>
+    // }
+    // let songList = songs.map((song) => {
+    //     <div key={song.id}>
+    //         <h3>{song.title}</h3>
+    //         <p>{song.time}</p>
+    //         <p>{song.artist}</p>
+    //         <button>Edit Song</button>
+    //         <button>Remove Song</button>
+    //         <button>Add to Favorites</button>
+
+    //     </div>
+    // })
 
   return (
     <div className="songs">
-        {songList}
+        {/* {songList} */}
     </div>
   );
 }
