3ed21134b8e2581db79a2571abe9f381a3e7c4a2
M. Brock Podgurski <mbpod10@gmail.com>
2020-07-16 15:02:41 -0400

    changes for deploy

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(logger("dev"));
-
 app.use("/api/songs", songsController);
+
+app.set("port", process.env.PORT || 8080);
+
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
