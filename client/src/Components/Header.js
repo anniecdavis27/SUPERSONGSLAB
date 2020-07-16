f7d13050a8acdd4449af7a44ea97044e506e4a0d
Annie Davis <anniecdavis27@gmail.com>
2020-07-15 22:58:24 -0400

    react branch created

---
+++ client/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+function Header() {
+  return (
+    <div className="Header">
+        <h1>TUNR.</h1>
+        <h3>FOR ALL YOUR PLAYLIST NEEDS</h3>
+    </div>
+  );
+}
+
+export default Header;
