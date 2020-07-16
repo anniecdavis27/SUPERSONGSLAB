56295ba2c2a5c1c0ec249dad166439f26df745b9
M. Brock Podgurski <mbpod10@gmail.com>
2020-07-15 22:46:02 -0400

    new repo

---
+++ controllers/songs.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const router = express.Router();
+const Song = require("../models/Song");
+
+//all songs
+router.get("/", (req, res) => {
+  Song.find({}, (error, songs) => {
+    if (error) console.log(error);
+    else res.json(songs);
+  });
+});
+
+// song by id
+router.get("/:id", (req, res) => {
+  Song.findById(req.params.id, (error, song) => {
+    if (error) console.log(error);
+    else res.json(song);
+  });
+});
+//post new song
+router.post("/", (req, res) => {
+  Song.create(req.body, (error, song) => {
+    if (error) console.log(error);
+    else res.json(song);
+  });
+});
+
+//update
+router.put("/:id", (req, res) => {
+  Song.findOneAndUpdate(
+    { _id: req.params.id },
+    req.body,
+    { new: true },
+    (error, song) => {
+      if (error) console.log(error);
+      else res.json(song);
+    }
+  );
+});
+
+//delete
+router.delete("/:id", (req, res) => {
+  Song.findByIdAndRemove(req.params.id, (error, song) => {
+    if (error) console.log(error);
+    else res.json(song);
+  });
+});
+
+//change from false to true
+router.put("/:id/fav", (req, res) => {
+  Song.findOneAndUpdate(
+    { _id: req.params.id },
+    { isFavorite: true },
+    { new: true },
+    (error, song) => {
+      if (error) console.log(error);
+      else res.json(song);
+    }
+  );
+});
+
+//add to favs
+router.put("/:id/fav", (req, res) => {
+  Song.findOneAndUpdate(
+    { _id: req.params.id },
+    { isFavorite: true },
+    { new: true },
+    (error, song) => {
+      if (error) console.log(error);
+      else res.json(song);
+    }
+  );
+});
+// remove favs
+router.put("/:id/fav/remove", (req, res) => {
+  Song.findOneAndUpdate(
+    { _id: req.params.id },
+    { isFavorite: false },
+    { new: true },
+    (error, song) => {
+      if (error) console.log(error);
+      else res.json(song);
+    }
+  );
+});
+
+module.exports = router;
